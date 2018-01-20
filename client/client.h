e1714603a2cedcbcc66c287daf5bd111d7fe8164
Diego <dgarci34@ucsc.edu>
2018-01-20 03:17:27 -0800

    added keys

--- client/client.h
+++ client/client.h
@@ -2,11 +2,29 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 #include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <curl/curl.h>
 
-//error codes
+#define key_name "Ocp-Apim-Subscription-Key"
+#define key1 "1f3639ec9c8c412297d971b87d44890a"
+#define key2 "37645043a2a147eaa0d8ee6b7d0cfc29"
+#define content_type "audio/wav; codec=audio/pcm; samplerate=16000"
+
 #define RECIEVE_ERROR 2
 #define SEND_ERROR 3
 #define READ_ERROR 4
+
+
+char * url;
+char * uri;
+
+char * response;
+
+char * out_message;
+
+static curlioerr my_ioctl(CURL *handle, curliocmd cmd, void *userp);
 
 int recieve_audio_object(void);
 int send_audio(void);
--- client/clientfunc.c
+++ client/clientfunc.c
@@ -1,16 +1,43 @@
 /*clientfunc.c
 here client functions are implmented*/
 #include "client.h"
+static curlioerr my_ioctl(CURL *handle, curliocmd cmd, void *userp)
+{
 
-int recieve_audio_object(void){
+    int *fdp = (int *)userp;
+    int fd = *fdp;
+
+    (void)handle; /* not used in here */
+
+    switch (cmd)
+    {
+    case CURLIOCMD_RESTARTREAD:
+        /* mr libcurl kindly asks as to rewind the read data stream to start */
+        if (-1 == lseek(fd, 0, SEEK_SET))
+            /* couldn't rewind */
+            return CURLIOE_FAILRESTART;
+
+        break;
+
+    default: /* ignore unknown commands */
+        return CURLIOE_UNKNOWNCMD;
+    }
+    return CURLIOE_OK; /* success! */
+}
+
+int recieve_audio_object(void)
+{
     return -1;
 }
-int send_audio(void){
+int send_audio(void)
+{
     return -1;
 }
-int convert_string(void){
+int convert_string(void)
+{
     return -1;
 }
-int convert_to_object(void){
+int convert_to_object(void)
+{
     return -1;
 }
